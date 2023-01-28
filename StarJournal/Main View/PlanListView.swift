a10aab8abb14f151bd7b91c3454c2cf1715efc81
Brad Slayter <SlayterDev@users.noreply.github.com>
2023-01-28 14:19:46 -0600

    Fix

--- StarJournal/Main View/PlanListView.swift
+++ StarJournal/Main View/PlanListView.swift
@@ -99,6 +99,6 @@ private let itemFormatter: DateFormatter = {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        PlanListView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
